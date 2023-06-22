e6b073eec4fd4232035cea5dc6fac6a302facf00
sonah5009 <sonah5009@gmail.com>
2023-06-23 00:32:06 +0900

    page.tsx 공부중

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,13 @@
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   // ...
+// };
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // ...
+// };
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // ...
+// };
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+const Mybutton = () => {
+  return <button>I am a button</button>;
+};
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Mybutton />
+    </div>
+  );
+};
+
+export default App;
